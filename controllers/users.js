9789431d5e8987e8c02cbe31a91ce1bb5c707209
Adrian <sayian9216@gmail.com>
2026-07-25 21:36:22 -0700

    added create and get user

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,6 @@
 const User = require("../models/user");
 
 // GET /users
-
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
@@ -18,8 +17,27 @@ const createUser = (req, res) => {
     .then((user) => res.status(201).send(user))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.messgae });
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
     });
 };
 
-module.exports = { getUsers, createUser };
+const getUser = (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: err.message });
+      } else if (err.name === "CastError") {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { getUsers, createUser, getUser };
