e155350bf9d403b1c224af68ceafe7d0e39eaa44
Adrian <sayian9216@gmail.com>
2026-07-27 17:58:09 -0700

    added clothingItem router

---
+++ controllers/clothingItems.js
@@ -0,0 +1,49 @@
+const ClothingItem = require("../models/clothingItem");
+
+const createItem = (req, res) => {
+  const { name, weather, imageURL } = req.body;
+
+  ClothingItem.create({ name, weather, imageURL })
+    .then((item) => {
+      console.log(item);
+      res.send({ data: item });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
+
+const updateItem = (req, res) => {
+  const { itemId } = req.params;
+  const { imageURl } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+
+  console.log(itemId);
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = { createItem, getItems, updateItem, deleteItem };
--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,5 +1,25 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const clothingItemSchema = new mongoose.Schema({});
+const clothingItem = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  weather: {
+    type: String,
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: true,
+    vvalidate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+});
 
-module.exports = mongoose.model("item", clothingItemSchema);
+module.exports = mongoose.model("clothingItem", clothingItem);
